36f6a5b63c57182238621aea4e6370930cd45dbc
Зенов Иван <116456650+cgsgiz3@users.noreply.github.com>
2023-06-18 14:20:40 +0300

    Update frag.glsl

--- rollup/icosahedron/frag.glsl
+++ rollup/icosahedron/frag.glsl
@@ -14,7 +14,7 @@ uniform float time;
 uniform materialBuffer
 {
   vec4 KaTrans;
-  vec4 Kd;
+  vec4 Kd1;
   vec4 KsPh;
 }; 
 uniform cameraBuffer
@@ -25,15 +25,21 @@ uniform cameraBuffer
   vec4 camup;                     
   vec4 localTimeglobalDeltaTime;  
 };
-vec3 shade() {            
-    vec3 L = normalize(vec3(5.0 * sin(localTimeglobalDeltaTime.x * 10.0), 5, 5));
-    vec3 LC = vec3(1, 1, 1);
-    vec3 V = normalize(pos - camloc.xyz);
-    vec3 N = normal;//vec3(0, 1, 0);
-    //N = faceforward(N, V, N);
-    return vec3(min(vec3(0.1), KaTrans.xyz) +
-            max(0.0, dot(N, L)) * Kd.xyz  * LC +
-            pow(max(0.0, dot(reflect(V, N), L)), KsPh.w) * KsPh.xyz * LC);   
+vec3 shade() 
+{            
+  vec3 Ka = KaTrans.xyz / 2.2;
+  vec3 Kd = Kd1.xyz / 2.2;
+  vec3 Ks = KsPh.xyz / 2.2;
+  float Ph = KsPh.w / 2.2;
+  float Trans = KaTrans.w / 2.2;
+  vec3 L = normalize(vec3(5.0 * sin(localTimeglobalDeltaTime.x), 5, 5));
+  vec3 LC = vec3(1, 1, 1);
+  vec3 V = normalize(pos - camloc.xyz);
+  vec3 N = normal;//vec3(0, 1, 0);
+  //N = faceforward(N, V, N);
+  return vec3(min(vec3(0.1), Ka) +
+          max(0.0, dot(N, L)) * Kd  * LC +
+          pow(max(0.0, dot(reflect(V, N), L)), Ph) * Ks * LC) * 2.2;   
 }
 void main()
 {
